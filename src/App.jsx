a8d0a6f95ea92a7823719cf22759495041ef7a0d
DenizAlakavuklar <deniz.kovanci@gmail.com>
2023-02-21 16:55:44 +0100

    Iteration 3 is completed.

--- src/App.jsx
+++ src/App.jsx
@@ -4,68 +4,59 @@ import contacts from './contacts.json'
 import icon from './assets/icon.jpg'
 
 function App() {
-  const [firstFive, setFirstFive] = useState (contacts.slice(0,5));
+  const [firstFive, setFirstFive] = useState(contacts.slice(0, 5));
+  const [afterFive, setAfterFive] = useState(contacts.slice(5));
+
   console.log(firstFive)
+  console.log(afterFive)
+
+  function addRandom() {
+
+    let randomIndex = Math.floor((Math.random() * afterFive.length))
+    let randomCeleb = afterFive[randomIndex]
+
+    setFirstFive([...firstFive, randomCeleb])
+    const filteredCelebs = afterFive.filter((currentCeleb) => {
+      if (currentCeleb.name !== randomCeleb.name) {
+        return currentCeleb
+      }
+    })
+    setAfterFive(filteredCelebs)
+  }
   return <div className='App'>
-<h1>Ironcontacts</h1>
-<table>
-<thead>
-  <tr>
-    <th>Picture</th>
-    <th>Name</th>
-    <th>Popularity</th>
-    <th>Won an Oscar</th>
-    <th>Won an Emmy</th>
-  </tr>
-</thead>
-<tbody>
-<tr>
-  <td>
-    <img className="photo" src={firstFive[0].pictureUrl} alt="celeb" />
-  </td>
-  <td> {firstFive[0].name}</td>
-  <td> {firstFive[0].popularity}</td>
-  {firstFive[0].wonOscar && <td> <img src={icon} alt="icon" className='icon'/></td>}
-  {firstFive[0].wonEmmy && <td> <img src={icon} alt="icon" className='icon'/></td>}
-  </tr>
-  <tr>
-  <td>
-    <img className="photo" src={firstFive[1].pictureUrl} alt="celeb" />
-  </td>
-  <td> {firstFive[1].name}</td>
-  <td> {firstFive[1].popularity}</td>
-  {firstFive[1].wonOscar && <td> <img src={icon} alt="icon" className='icon'/></td>}
-  {firstFive[1].wonEmmy && <td> <img src={icon} alt="icon" className='icon'/></td>}
-  </tr>
-  <tr>
-  <td>
-    <img className="photo" src={firstFive[2].pictureUrl} alt="celeb" />
-  </td>
-  <td> {firstFive[2].name}</td>
-  <td> {firstFive[2].popularity}</td>
-  {firstFive[2].wonOscar && <td> <img src={icon} alt="icon" className='icon'/></td>}
-  {firstFive[2].wonEmmy && <td> <img src={icon} alt="icon" className='icon'/></td>}
-  </tr>
-  <tr>
-  <td>
-    <img className="photo" src={firstFive[3].pictureUrl} alt="celeb" />
-  </td>
-  <td> {firstFive[3].name}</td>
-  <td> {firstFive[3].popularity}</td>
-  {firstFive[3].wonOscar && <td> <img src={icon} alt="icon" className='icon'/></td>}
-  {firstFive[3].wonEmmy && <td> <img src={icon} alt="icon" className='icon'/></td>}
-  </tr>
-  <tr>
-  <td>
-    <img className="photo" src={firstFive[4].pictureUrl} alt="celeb" />
-  </td>
-  <td> {firstFive[4].name}</td>
-  <td> {firstFive[4].popularity}</td>
-  {firstFive[4].wonOscar && <td> <img src={icon} alt="icon" className='icon'/></td>}
-  {firstFive[4].wonEmmy && <td> <img src={icon} alt="icon" className='icon'/></td>}
-  </tr>
-</tbody>
-</table>
+
+    <h1>Ironcontacts</h1>
+
+    <button type="submit" onClick={addRandom} >
+      Add
+    </button>
+
+    <table>
+      <thead>
+        <tr>
+          <th>Picture</th>
+          <th>Name</th>
+          <th>Popularity</th>
+          <th>Won an Oscar</th>
+          <th>Won an Emmy</th>
+        </tr>
+      </thead>
+      <tbody>
+        {firstFive.map((celeb) => {
+          return (
+            <tr>
+              <td>
+                <img className="photo" src={celeb.pictureUrl} alt="celeb" />
+              </td>
+              <td> {celeb.name}</td>
+              <td> {celeb.popularity}</td>
+              {celeb.wonOscar && <td> <img src={icon} alt="icon" className='icon' /></td>}
+              {celeb.wonEmmy && <td> <img src={icon} alt="icon" className='icon' /></td>}
+            </tr>
+          )
+        })}
+      </tbody>
+    </table>
 
   </div>
 }
